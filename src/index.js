68b9211dc4de797bd4b0b5df3b985448bed8349e
Manh8008 <125587030+Manh8008@users.noreply.github.com>
2024-08-31 15:34:28 +0700

    update

--- src/index.js
+++ src/index.js
@@ -10,12 +10,7 @@ const port = 5000;
 
 dotenv.config();
 
-app.use(
-    cors({
-        origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-        credentials: true,
-    }),
-);
+app.use(cors());
 
 const db = require('./config/db');
 db.connect();
